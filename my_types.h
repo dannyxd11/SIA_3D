dc96d57924c8a7b1e215ed9801c9719c3cee7361
Dan Whitehouse <dannyxd@me.com>
2016-11-29 13:36:06 +0000

    Header files required for interfacing with MatLab blas functions

---
+++ my_types.h
@@ -0,0 +1,13 @@
+/* Declare user defined types */
+
+#ifndef MY_TYPES_H
+#define MY_TYPES_H
+
+typedef unsigned long ulng;
+typedef unsigned uint;
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef double dbl;
+typedef long lng;
+
+#endif
