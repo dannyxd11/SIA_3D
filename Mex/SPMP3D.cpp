348e03faeebc1189354ac1e1a68275a7b1ce3ac0
Dan Whitehouse <dannyxd@me.com>
2017-02-02 12:53:30 +0000

    Few changes regarding memory / few bugs. Still not running further than 1 iteration.

--- Mex/SPMP3D.cpp
+++ Mex/SPMP3D.cpp
@@ -21,7 +21,7 @@ double sumOfSquares(double *matrix, int *dimensions) {
 
 
 int max(double *matrix, int *dimensions) {
-    double maxValue = std::abs(matrix[0]);
+    double maxValue = 0;
     int n1 = -1;
     for (int k = 0; k < dimensions[0] * dimensions[1] * dimensions[2]; k++) {
         if (std::abs(matrix[k]) > maxValue) {
@@ -66,117 +66,118 @@ void ind2sub(int *dimensions, int index, int *q) {
 
 void SPMP3D(double* f, int* fDim, double* Vx, int* VxDim, double* Vy, int* VyDim, double* Vz, int* VzDim,
         double tol, int No, double toln, int lstep, int Max, int Maxp, int* indx, int* indy, int* indz,
-        double* h, int* hDim, double* c, int* cDim, double* Set_ind, int* numat){
+        double* h, int* hDim, double* c, int* cDim, int* Set_ind, int* numat){
 // h, c, set_ind
 
-for(int i = 0; i < VxDim[0] * VyDim[0] * VzDim[0]; i++){
-    c[0] = 0;
-}
+    for(int i = 0; i < VxDim[0] * VyDim[0] * VzDim[0]; i++){
+        c[0] = 0;
+    }
 
-int Lx = VxDim[0];
-int Nx = VxDim[1];
-int Ly = VyDim[0];
-int Ny = VyDim[1];
-int Lz = VzDim[0];
-int Nz = VzDim[1];
+    int Lx = VxDim[0];
+    int Nx = VxDim[1];
+    int Ly = VyDim[0];
+    int Ny = VyDim[1];
+    int Lz = VzDim[0];
+    int Nz = VzDim[1];
 
-double delta = 1 / (Lx * Ly * Lz);
-int Nxyz = Lx * Ly * Lz;
-
-//if (nargin<12) | (isempty(indy)==1)  indz=[];end
-//if (nargin<11) | (isempty(indy)==1)  indy=[];end
-//if (nargin<10) | (isempty(indx)==1)  indx=[];end
-//if (nargin<9) | (isempty(Maxp)==1)  Maxp=7000;end
-//if (nargin<8)  | (isempty(Max)==1)   Max=7000;end
-//if (nargin<7)  | (isempty(lstep)==1) lstep=0;end
-//if (nargin<6)  | (isempty(toln)==1)  tolnu=1e-3;end
-//if (nargin<5)  | (isempty(No)==1)    No=Nxyz;end
-//if (nargin<4)  | (isempty(tol)==1)   tol=5e-4*sum(sum(sum(abs(f).^2)))*delta;end;
-
-double* cp = new double[Nx * Ny * Nz]();
-int cpDim[] = {Lx, Ly, Lz};
-double* cc = new double[Nx * Ny * Nz]();
-int ccDim[] = {Lx, Ly, Lz};
+    int Nxyz = Lx * Ly * Lz;
+    double delta = 1.0 / Nxyz;
 
 
-int MaxInd = max(f, fDim);
-int MaxInt = f[MaxInd];
-double* Di1; //= new double[ReDim[0] * ReDim[1]];
-double* Di2; // = new double[ReDim[0] * ReDim[1]];
-double* Di3; // = new double[ReDim[0] * ReDim[1]];
-numat[0] = 0;
-Set_ind = new double[3 * Max];
+    //if (nargin<12) | (isempty(indy)==1)  indz=[];end
+    //if (nargin<11) | (isempty(indy)==1)  indy=[];end
+    //if (nargin<10) | (isempty(indx)==1)  indx=[];end
+    //if (nargin<9) | (isempty(Maxp)==1)  Maxp=7000;end
+    //if (nargin<8)  | (isempty(Max)==1)   Max=7000;end
+    //if (nargin<7)  | (isempty(lstep)==1) lstep=0;end
+    //if (nargin<6)  | (isempty(toln)==1)  tolnu=1e-3;end
+    //if (nargin<5)  | (isempty(No)==1)    No=Nxyz;end
+    //if (nargin<4)  | (isempty(tol)==1)   tol=5e-4*sum(sum(sum(abs(f).^2)))*delta;end;
+
+    double* cp = new double[Nx * Ny * Nz]();
+    int cpDim[] = {Lx, Ly, Lz};
+    double* cc = new double[Nx * Ny * Nz]();
+    int ccDim[] = {Nx, Ny, Nz};
+
+
+    int MaxInd = max(f, fDim);
+    double MaxInt = f[MaxInd];
+    int* Di1; //= new double[ReDim[0] * ReDim[1]];
+    int* Di2; // = new double[ReDim[0] * ReDim[1]];
+    int* Di3; // = new double[ReDim[0] * ReDim[1]];
+    numat[0] = 0;
+    Set_ind = new int[3 * Max]();
 
 
 
-double *Dix = new double[Nx];
-double *Diy = new double[Ny];
-double *Diz = new double[Nz];
+    double *Dix = new double[Nx];
+    double *Diy = new double[Ny];
+    double *Diz = new double[Nz];
 
-int *DixDim = new int[3];
-int *DiyDim = new int[3];
-int *DizDim = new int[3];
+    int *DixDim = new int[3];
+    int *DiyDim = new int[3];
+    int *DizDim = new int[3];
 
-setDimensions(1, Nx, 1, DixDim);
-setDimensions(1, Ny, 1, DiyDim);
-setDimensions(1, Nz, 1, DizDim);
+    setDimensions(1, Nx, 1, DixDim);
+    setDimensions(1, Ny, 1, DiyDim);
+    setDimensions(1, Nz, 1, DizDim);
 
-initiateRangeVector(Dix, Nx);
-initiateRangeVector(Diy, Ny);
-initiateRangeVector(Diz, Nz);
-
-
-int numind = 0; //todo numel(indx);
-numind = 0;
+    initiateRangeVector(Dix, Nx);
+    initiateRangeVector(Diy, Ny);
+    initiateRangeVector(Diz, Nz);
 
 
-if(sumOfSquares(f, fDim) * delta < 1e-9){
-    c = new double[0];
-    return;
-}
-
-double *Re = new double[fDim[0] * fDim[1] * fDim[2]];
-int *ReDim = new int[3];
-setDimensions(fDim, ReDim);
-memcpy(Re, f, ReDim[0] * ReDim[1] * ReDim[2] * sizeof(double));
-
-double tol2 = 1e-9;
-int imp = 0;
-int Maxit2 = 0;
-
-if (lstep == -1)imp = 1;
-if (imp == 1)lstep = 0;
-if (lstep == 0) {
-    Maxit2 = 1;
-    lstep = Max;
-}else {
-    Maxit2 = Max / lstep;
-}
-
-/*
- *  Constant depending on dictionary size, move allocation outside loop to reduce unnessecary overhead
- */
-int tempVxCol[] = {VxDim[0], 1, 1};
-int tempVyCol[] = {VyDim[0], 1, 1};
-int tempVzCol[] = {VzDim[0], 1, 1};
-int h_newDim[] = {VxDim[0], VyDim[0], VzDim[0]};
-int cscraDim[]= {1,1,1};
-double *h_new = new double[h_newDim[0] * h_newDim[1] * h_newDim[2]];
-int *q = new int[3];
-double* Set_ind_trans;
-int it;
+    int numind = 0; //todo numel(indx);
+    numind = 0;
 
 
-double* plane;
-double* col;
-double* Row = new double[VxDim[0]];
-int ColDim[] = {VxDim[0], 1, 1};
-int RowDim[] = {1, VxDim[0], 1};
-int planeDim[] = {ReDim[0], ReDim[1], 1};
-double* multResult1 = new double[VxDim[0]];
-int multResult1Dim[] = {1, VxDim[0], 1};
-double* multResult2 = new double[1];
-int multResult2Dim[] = {1, 1, 1};
+    if(sumOfSquares(f, fDim) * delta < 1e-9){
+        c = new double[0];
+        return;
+    }
+
+    double *Re = new double[fDim[0] * fDim[1] * fDim[2]];
+    int *ReDim = new int[3];
+    setDimensions(fDim, ReDim);
+    memcpy(Re, f, ReDim[0] * ReDim[1] * ReDim[2] * sizeof(double));
+
+    double tol2 = 1e-9;
+    int imp = 0;
+    int Maxit2 = 0;
+
+    if (lstep == -1)imp = 1;
+    if (imp == 1)lstep = 0;
+    if (lstep == 0) {
+        Maxit2 = 1;
+        lstep = Max;
+    }else {
+        Maxit2 = Max / lstep;
+    }
+
+    /*
+     *  Constant depending on dictionary size, move allocation outside loop to reduce unnessecary overhead
+     */
+    int tempVxCol[] = {VxDim[0], 1, 1};
+    int tempVyCol[] = {VyDim[0], 1, 1};
+    int tempVzCol[] = {VzDim[0], 1, 1};
+    int h_newDim[] = {VxDim[0], VyDim[0], VzDim[0]};
+    int cscraDim[]= {1,1,1};
+    double *h_new = new double[h_newDim[0] * h_newDim[1] * h_newDim[2]];
+    int *q = new int[3];
+    int* Set_ind_trans = new int[3];
+    int it;
+
+
+    double* plane;
+    double* col;
+    double* Row = new double[VxDim[0]];
+    int ColDim[] = {VxDim[0], 1, 1};
+    int RowDim[] = {1, VxDim[0], 1};
+    int planeDim[] = {ReDim[0], ReDim[1], 1};
+    double* multResult1 = new double[VxDim[0]];
+    int multResult1Dim[] = {1, VxDim[0], 1};
+    double* multResult2 = new double[1];
+    int multResult2Dim[] = {1, 1, 1};
 
     for (it = 0; it < Maxit2; it++) {
         for (int s = 0; s < lstep; s++) {
@@ -206,7 +207,7 @@ int multResult2Dim[] = {1, 1, 1};
 
                 }
             } else {
-                IP3D(Re, ReDim, Vx, VxDim, Vy, VyDim, Vz, VzDim, cc, ccDim);
+                IP3d(Re, ReDim, Vx, VxDim, Vy, VyDim, Vz, VzDim, cc, ccDim);
                 int maxind = max(cc, ccDim);
 
                 ind2sub(ccDim, maxind, q);
@@ -217,13 +218,13 @@ int multResult2Dim[] = {1, 1, 1};
                 }
             }
 
-            int vq[] = {q[1], q[2], q[3]};
+            int vq[] = {q[0], q[1], q[2]};
 
-            if (numat == 0) {
+            if (numat[0] == 0) {
                 Set_ind[0] = vq[0];
                 Set_ind[1] = vq[1];
                 Set_ind[2] = vq[2];
-                numat += 1;
+                numat[0] += 1;
             } else {
                 int exists = 0;
                 /*
@@ -246,7 +247,7 @@ int multResult2Dim[] = {1, 1, 1};
             }
 
 
-            double cscra = get3DElement(cc, ccDim, q[1], q[2], q[3]);
+            double cscra = get3DElement(cc, ccDim, q[0], q[1], q[2]);
 
 
             hnew3d(&cscra, cscraDim,
@@ -255,7 +256,7 @@ int multResult2Dim[] = {1, 1, 1};
                        getCol(Vz, VzDim, q[2]), tempVzCol,
                        h_new, h_newDim);
 
-            set3DElement(cp, cpDim, q[1], q[2], q[3], get3DElement(cp, cpDim, q[1], q[2], q[3]) + cscra);
+            set3DElement(cp, cpDim, q[0], q[1], q[2], get3DElement(cp, cpDim, q[0], q[1], q[2]) + cscra);
 
             for(int k = 0; k < h_newDim[0] * h_newDim[1] * h_newDim[2]; k++) {
                 h[k] += h_new[k];
@@ -276,7 +277,7 @@ int multResult2Dim[] = {1, 1, 1};
 
         if (imp != 1) {
             delete [] Set_ind_trans;
-            Set_ind_trans = new double[3 * numat[0]];
+            Set_ind_trans = new int[3 * numat[0]];
             int Set_ind_trans_dim[] = {numat[0], 3, 1};
             int Set_ind_dim[] = {3, numat[0], 1};
 
@@ -317,33 +318,47 @@ int multResult2Dim[] = {1, 1, 1};
             for (int n = 0; n < l; n++) {
                 set3DElement(cp, cpDim, Set_ind[n * 3], Set_ind[n * 3 +1], Set_ind[n * 3 + 2], c[n]);
             }
+
+            delete [] VxTemp;
+            delete [] VyTemp;
+            delete [] VzTemp;
         }
 
         double nore = sumOfSquares(Re, ReDim) * delta;
 
         if (numat[0] >= No || (nore < tol)) break;
     }
-        if ((lstep != Max) && (it==Maxit2)){
-            mexPrintf("%s Maximum number of iterations has been reached");
-        }
 
-    /*
-     * Set_ind was stored width major, Matlab expects it height major- Transpose.
-     */
-
-    double* tempSetT = new double[numat[0] * 3];
-    int tempSetDimT[] = {numat[0], 3, 1};
-    int tempSetDim[] = {3, numat[0], 1};
-
-    transpose(Set_ind, tempSetDim, tempSetT, tempSetDimT);
-
-    delete [] Set_ind;
-    Set_ind = tempSetT;
+    if ((lstep != Max) && (it==Maxit2)){
+        mexPrintf("%s Maximum number of iterations has been reached");
+    }
 
 
     delete [] Row;
     delete [] multResult1;
     delete [] multResult2;
+
+    delete [] cp;
+    delete [] cc;
+
+    delete [] Dix;
+    delete [] Diy;
+    delete [] Diz;
+
+    delete [] DixDim;
+    delete [] DiyDim;
+    delete [] DizDim;
+
+    delete [] Re;
+    delete [] ReDim;
+
+    delete [] h_new;
+    delete [] q;
+
+    delete [] Row;
+    delete [] multResult1;
+    delete [] multResult2;
+
 }
 
 
@@ -358,11 +373,11 @@ void mexFunction(int nlhs, mxArray *plhs[],
     double *Vy = mxGetPr(prhs[2]);
     double *Vz = mxGetPr(prhs[3]);
     double tol = mxGetPr(prhs[4])[0];
-    int No = ((int*)mxGetData(prhs[5]))[0];
+    int No = (int)(mxGetPr(prhs[5])[0]);
     double toln = mxGetPr(prhs[6])[0];
-    int lstep = ((int*)mxGetData(prhs[7]))[0];
-    int Max = ((int*)mxGetData(prhs[8]))[0];
-    int Maxp = ((int*)mxGetData(prhs[9]))[0];
+    int lstep = (int)(mxGetPr(prhs[7])[0]);
+    int Max = (int)(mxGetPr(prhs[8])[0]);
+    int Maxp = (int)(mxGetPr(prhs[9])[0]);
     int* indx = (int*)mxGetData(prhs[10]);
     int* indy = (int*)mxGetData(prhs[10]);
     int* indz = (int*)mxGetData(prhs[10]);
@@ -380,9 +395,9 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
 
 
-    double* Set_ind;
+    int* Set_ind;
     double* c = new double[VxDim[0] * VyDim[0] * VzDim[0]];
-    int cDim[] = {VxDim[0], VyDim[0], VzDim[0]};
+    int cDim[3] = {VxDim[0], VyDim[0], VzDim[0]};
     int numat = 0;
     int cDims = 3;
 
@@ -390,13 +405,27 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
 
 
-    int Set_ind_dims = 3; int Set_ind_dim[] = {numat, 3, 1};
+    int Set_ind_dims = 3, Set_ind_dim[3] = {numat, 3, 1};
 
     plhs[1] = mxCreateNumericArray(cDims, cDim, mxDOUBLE_CLASS, mxREAL);
-    plhs[2] = mxCreateNumericArray(Set_ind_dims, Set_ind_dim, mxDOUBLE_CLASS, mxREAL);
+    plhs[2] = mxCreateNumericArray(Set_ind_dims, Set_ind_dim, mxINT32_CLASS, mxREAL);
+    //plhs[2] = mxCreateNumericArray(Set_ind_dims, Set_ind_dim, mxDOUBLE_CLASS, mxREAL);
+
+    int* tempSetT = new int[numat * 3];
+    int tempSetDimT[] = {numat, 3, 1};
+    int tempSetDim[] = {3, numat, 1};
+
+    transpose(Set_ind, tempSetDim, tempSetT, tempSetDimT);
+
+    //delete [] Set_ind;
+    //Set_ind = tempSetT;
 
     memcpy(plhs[1], c, cDim[0] * cDim[1] * cDim[2] * sizeof(double));
-    memcpy(plhs[2], Set_ind, numat * 3 * sizeof(double));
+    memcpy(plhs[2], tempSetT, numat * 3 * sizeof(int));
+
+    delete [] c;
+    //delete [] Set_ind;
+	delete [] tempSetT;
 }
 
 
