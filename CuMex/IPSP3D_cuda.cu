0bdec25f70dfecd32131d5b284b127ce9861aebc
Whitehouse <whitehdg@easmath14925dl.campus.aston.ac.uk>
2017-03-03 19:48:56 +0000

    IPSP3D Function now works.

--- CuMex/IPSP3D_cuda.cu
+++ CuMex/IPSP3D_cuda.cu
@@ -13,55 +13,111 @@ inline void cudaAssert(cudaError_t code, const char *file, int line){
 	if (code != cudaSuccess){ fprintf(stderr, "CudaAssert %s %s %d\n", cudaGetErrorString(code), file, line); exit(code);}
 }
 
-
+void printDeviceDetails(){
+    std::cout << "Device Details... \n" << std::endl;
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n",
+               prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n",
+               prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n",
+               2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        int driverVersion = 0, runtimeVersion = 0;
+        cudaRuntimeGetVersion(&runtimeVersion);
+        cudaDriverGetVersion(&driverVersion); // http://rcs.bu.edu/examples/gpu/tutorials/deviceQuery/deviceQuery.cpp
+        printf("  Driver Version: %d.%d\n",driverVersion/1000, (driverVersion%100)/10);
+        printf("  Runtime Version: %d.%d\n",runtimeVersion/1000, (runtimeVersion%100)/10);
+        printf("  CUDA Version: %d.%d\n\n",  prop.major, prop.minor);
+    }
+}
 
 class h_Matrix{
 public:
     double* elements;
     double* devElements;
     int height, width, depth;
-    h_Matrix(int height, int width, int depth) : height(height), width(width), depth(depth) { elements = new double[height*width*depth](); };
-    h_Matrix(double* elements, int height, int width, int depth) : height(height), width(width), depth(depth), elements(elements) {};
-    int numel (){return height * width * depth;};
+    __host__ __device__ h_Matrix() : height(1), width(1), depth(1) {};
+    __host__ __device__ h_Matrix(int height, int width, int depth) : height(height), width(width), depth(depth) { elements = new double[height*width*depth](); };
+    __host__ __device__ h_Matrix(double* elements, int height, int width, int depth) : height(height), width(width), depth(depth), elements(elements) {};
+    __host__ __device__ int numel (){return height * width * depth;};
+    __host__ __device__ double* getColDouble(int i){return &elements[height * i];}
+    __host__ __device__ double* getElement(int i, int j){return &elements[i * height + j];};
+    __host__ __device__ void setElement(int i, int j, double value){elements[i * height + j] = value;};
+    __host__ __device__ void setElement(int i, double value){elements[i] = value;};
+    __host__ __device__ double* getElement(int i){return &elements[i];};
+    __host__ __device__ h_Matrix getCol(int i){h_Matrix newMatrix(getColDouble(i),height, 1, 1); return newMatrix;};
+    __host__ __device__ h_Matrix getPlane(int i){h_Matrix newMatrix(&elements[height * width * i], height, width, 1); return newMatrix;};
 };
 
-__device__ void multiplyCuda(double* a, double* b, double* c, int lda, int ldb, int ldc, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
-	    int y = threadIdx.y;
-	    int x = threadIdx.x;
 
-		if (y < m && x < n){
-	       double cellSum = 0;
-				 if (op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_N){
-					 for(int i = 0; i < k; i++){
-						cellSum += a[lda * i + y] * b[ldb * x + i] * alpha[0];
-					 }
-				 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_N){
-					 for(int i = 0; i < k; i++){
-					 	cellSum += a[lda * y + i] * b[ldb * x + i] * alpha[0];
-					 }
-				 }else if(op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_T){
-					 for(int i = 0; i < k; i++){
-					 	cellSum += a[lda * i + y] * b[ldb * i + x] * alpha[0];
-					 }
-				 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_T){
-					 for(int i = 0; i < k; i++){
-					 	cellSum += a[lda * y + i] * b[ldb * i + x] * alpha[0];
-					 }
+
+__device__ void multiplyCuda(double* a, double* b, double* c, int lda, int ldb, int ldc, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha, double* beta){
+	// m is number of rows of op(a)
+	// n is number of cols of op(b)
+	// k is number of rows of a and width of c)
+
+	int y = threadIdx.y; //col
+	int x = threadIdx.x; //row
+
+	if (y < m && x < n){
+	   double cellSum = 0;
+			 if (op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_N){
+				 for(int i = 0; i < k; i++){
+					cellSum += a[lda * i + y] * b[ldb * x + i] * alpha[0];
+					//printf("\ni: %d, y: %d, x: %d, lda: %d, ldb: %d, alpha: %f, temp: %f, cellSum: %f, aVal: %f, bVal: %f, aind %d, bind: %d", i, y, x, lda, ldb, alpha[0], a[lda * i + y] * b[ldb * x + i] * alpha[0], cellSum, a[lda * i + y], b[ldb * x + i], lda*i+y, ldb*x+i);
 				 }
-				c[ldc * x + y] = cellSum;
-				printf("ThreadID %d,%d, A: %f, / %d    B: %f, / %d    C: %f / %d\n", x,y,a[lda * x + y],lda * x + y,b[ldb * y + x],ldb * y + x,c[ldc * y + x],ldc * y + x);
-	   }
+			 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_N){
+				 for(int i = 0; i < k; i++){
+					cellSum += a[lda * y + i] * b[ldb * x + i] * alpha[0];
+				 }
+			 }else if(op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_T){
+				 for(int i = 0; i < k; i++){
+					cellSum += a[lda * i + y] * b[ldb * i + x] * alpha[0];
+				 }
+			 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_T){
+				 for(int i = 0; i < k; i++){
+					cellSum += a[lda * y + i] * b[ldb * i + x] * alpha[0];
+				 }
+			 }
+			c[ldc * y + x] = beta[0] * c[ldc * y + x] + cellSum;
+			//printf("\nThreadID %d,%d, A: %f, / %d    B: %f, / %d    C: %f / %d \t K: %d, M: %d, N: %d\n", x,y,a[lda * x + y],lda * x + y,b[ldb * y + x],ldb * y + x,c[ldc * y + x],ldc * y + x, k, m, n);
+   }
 }
 
-__global__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
+__device__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha, double* beta){
 	int lda = a->height;
 	int ldb = b->height;
 	int ldc = m;
-	multiplyCuda(a->elements, b->elements, c->elements,lda, ldb, ldc, m, n, k, op1, op2, alpha);
+	multiplyCuda(a->elements, b->elements, c->elements,lda, ldb, ldc, m, n, k, op1, op2, alpha, beta);
+	c->height = ldc;
+	c->width = k;
 }
 
-__global__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
-   	 int m;
+
+
+__device__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, double* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha, double* beta){
+	int lda = a->height;
+	int ldb = b->height;
+	int ldc = m;
+	multiplyCuda(a->elements, b->elements, c,lda, ldb, ldc, m, n, k, op1, op2, alpha, beta);
+}
+
+__device__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
+	double* beta = new double[1]();
+	matrixMultiplyCuda(a, b, c, m, n, k, op1, op2, alpha, beta);
+}
+
+
+
+
+__device__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
+  	 int m;
      int n;
 
 	 if(op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_N){
@@ -94,7 +150,7 @@ __global__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, cublas
 		 }
 	 }
 
-   	 int k = a->height;
+  	 int k = a->height;
 	 if(op1 == CUBLAS_OP_N){
 	 	k = a->width;
 	 }
@@ -103,32 +159,16 @@ __global__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, cublas
 	 int leadingDimensionB = b->height;
 	 int leadingDimensionC = a->height;
 
-//
-//   if (y < m && x < n){
-//       double cellSum = 0;
-//			 if (op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_N){
-//				 for(int i = 0; i < k; i++){
-//					cellSum += a->elements[leadingDimensionA * i + y] * b->elements[leadingDimensionB * x + i];
-//				 }
-//			 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_N){
-//				 for(int i = 0; i < k; i++){
-//				 	cellSum += a->elements[leadingDimensionA * y + i] * b->elements[leadingDimensionB * x + i];
-//				 }
-//			 }else if(op1 == CUBLAS_OP_N && op2 == CUBLAS_OP_T){
-//				 for(int i = 0; i < k; i++){
-//				 	cellSum += a->elements[leadingDimensionA * i + y] * b->elements[leadingDimensionB * i + x];
-//				 }
-//			 }else if(op1 == CUBLAS_OP_T && op2 == CUBLAS_OP_T){
-//				 for(int i = 0; i < k; i++){
-//				 	cellSum += a->elements[leadingDimensionA * y + i] * b->elements[leadingDimensionB * i + x];
-//				 }
-//			 }
-//			c->elements[leadingDimensionC * x + y] = cellSum;
-//			printf("ThreadID %d,%d, A: %f, / %d    B: %f, / %d    C: %f / %d\n", x,y,a->elements[leadingDimensionA * x + y],leadingDimensionA * x + y,b->elements[leadingDimensionB * y + x],leadingDimensionB * y + x,c->elements[leadingDimensionC * y + x],leadingDimensionC * y + x);
-//   }
+	 double* beta = new double[1]();
+	 multiplyCuda(a->elements, b->elements, c->elements, leadingDimensionA, leadingDimensionB, leadingDimensionC, m, n, k, op1, op2, alpha, beta);
+}
 
-	 multiplyCuda(a->elements, b->elements, c->elements, leadingDimensionA, leadingDimensionB, leadingDimensionC, m, n, k, op1, op2, alpha);
+__global__ void matrixMultiplyCudaKernel(h_Matrix* a, h_Matrix* b, h_Matrix* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
+	matrixMultiplyCuda(a, b, c, m, n, k, op1, op2, alpha);
+}
 
+__global__ void matrixMultiplyCudaKernel(h_Matrix* a, h_Matrix* b, h_Matrix* c, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
+	matrixMultiplyCuda(a, b, c, op1 ,op2, alpha);
 }
 
 
@@ -141,6 +181,7 @@ h_Matrix* copyMatrixToDevice(h_Matrix *hostMatrix){
     // Allocate Space on Device for Array
     cudaCheck(cudaMalloc(&deviceElements, hostMatrix->numel() * sizeof(double)));
     hostMatrix->devElements = deviceElements;
+
     // Allocate Space on Device for Class Container
     cudaCheck(cudaMalloc((void **)&deviceMatrix, sizeof(h_Matrix)));
 
@@ -161,50 +202,69 @@ h_Matrix* copyMatrixToDevice(h_Matrix *hostMatrix){
     return deviceMatrix;
 }
 
+void copyMatrixToHost(h_Matrix *hostMatrix, h_Matrix *deviceMatrix){
 
-__global__
-void d_IPSP3d(cublasHandle_t handle, h_Matrix* re, h_Matrix* v1, h_Matrix* v2, h_Matrix* v3, h_Matrix* cc){
+    double *hostElements;
 
-	__shared__ int n1, l3;
+	// Copy Matrix Properties
+    cudaCheck(cudaMemcpy(&hostMatrix->height, &deviceMatrix->height, sizeof(int),cudaMemcpyDeviceToHost));
+    cudaCheck(cudaMemcpy(&hostMatrix->width, &deviceMatrix->width, sizeof(int),cudaMemcpyDeviceToHost));
+    cudaCheck(cudaMemcpy(&hostMatrix->depth, &deviceMatrix->depth, sizeof(int),cudaMemcpyDeviceToHost));
 
-	if(threadIdx.x == 0){
+
+    // Copy Elements from Device to Host)
+    if(!hostMatrix->elements){
+    	delete [] hostMatrix->elements;
+    }
+
+    hostElements = new double[hostMatrix->numel()];
+    hostMatrix->elements = hostElements;
+
+    // Copy the value (address) of elements on the device to the host devElements attribute
+    cudaCheck(cudaMemcpy(&hostMatrix->devElements, &deviceMatrix->elements, sizeof(double *), cudaMemcpyDeviceToHost));
+
+    // Copy the elements from the device to the elements container on the host
+    cudaCheck(cudaMemcpy(hostMatrix->elements, hostMatrix->devElements,sizeof(double) * hostMatrix->numel(),cudaMemcpyDeviceToHost));
+
+}
+
+h_Matrix copyMatrixToHost(h_Matrix *deviceMatrix){
+    //Allocate Space on Host for Class Container)
+	h_Matrix hostMatrix;
+    copyMatrixToHost(&hostMatrix, deviceMatrix);
+    return hostMatrix;
+}
+
+
+
+
+__global__ void d_IPSP3d(h_Matrix* re, h_Matrix* v1, h_Matrix* v2, h_Matrix* v3, h_Matrix* cc){
+
+	double scalar = 1;
+	__shared__ int n1, l3;)
+    __shared__ h_Matrix aMatrix;
+	__shared__ double *aMatrixElements;
+
+	if(threadIdx.x == 0 && threadIdx.y == 0){
 		n1 = v1->width;
 		l3 = v3->height;
+		aMatrixElements = new double[re->width]();
+		aMatrix.elements = aMatrixElements;
+		aMatrix.width =; 8; aMatrix.height = aMatrix.depth = 1;
 	}
 
-
- //   cublasStatus_t status;
-
- //   __shared__ double* c;
- //   __shared__ double scalar;
- //   __syncthreads();
-
-//    if(threadIdx.x == 0){
-//    	 c = new double[v2->height * re->width]();
-//    	 scalar = 1;
-//    	 printf("%f\n", cc->elements[0]);
-//__global__ void matrixMultiplyCuda(h_Matrix* a, h_Matrix* b, h_Matrix* c, int m, int n, int k, cublasOperation_t op1, cublasOperation_t op2, double* alpha){
-//    	 //status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, 1, v2->height, re->height, &scalar, &v2->elements[0], v2->height, &re->elements[0], re->height, 0, c, v2->height);
-//    	 printf("%f", c[0]);
-//    	 if (status != CUBLAS_STATUS_SUCCESS){printf("Cublas DGEMM failure (v2xre), dan you fool\n"); return;}
-//    	 if (status == CUBLAS_STATUS_SUCCESS){printf("Dan you genius\n"); return;}
-//    	 printf("\n%f, %d", c[0], status);
-//    	 cc->elements[0] = c[0];
-//    }
-
-
-
-/*
-    //    for(int i = 0; i < n1; i ++){
-//
-//        for(int j = 0; j < l3; j++){
-//            // V2(:,n)'*Re(:,:,zk)'  // height of V2 (1 since its a row) width of Re (Transposed so height)
-//            double* c = new double(v2->height * re->width);
-//            cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, 1, re->width, re->height, 1, v2->elements.data(), v2->height, re->elements.data(), re->height, 0, c,v2->height);
-//
-//        }
-//    }
-*/
+		for(int i = 0; i < n1; i++){
+			cc->setElement(i, 0.0);
+			for(int j = 0; j < l3; j++){
+				h_Matrix v1Col = v1->getCol(i);
+				h_Matrix v2Col = v2->getCol(i);
+				matrixMultiplyCuda(&v1Col, &re->getPlane(j), &aMatrix, 1, re->width, v1->height, CUBLAS_OP_T, CUBLAS_OP_N, &scalar);
+				__syncthreads();
+				matrixMultiplyCuda(&aMatrix, &v2Col, cc->getElement(i), 1, 1, aMatrix.width, CUBLAS_OP_N, CUBLAS_OP_N, v3->getElement(i, j), &scalar);
+				__syncthreads();
+				//if (threadIdx.x == 0){printf("i: %d, j: %d, V3 Val:%f, \n", i, j, v3->getElement(i,j)[0]);}
+			}
+		}
     return;
 }
 
@@ -218,41 +278,19 @@ int main() {
     double reElements[] = {8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6};
     double* ccElements = new double[40]();
 
-
-
-    std::cout << "Device Details... \n" << std::endl;
-    int nDevices;
-    cudaGetDeviceCount(&nDevices);
-    for (int i = 0; i < nDevices; i++) {
-        cudaDeviceProp prop;
-        cudaGetDeviceProperties(&prop, i);
-        printf("Device Number: %d\n", i);
-        printf("  Device name: %s\n", prop.name);
-        printf("  Memory Clock Rate (KHz): %d\n",
-               prop.memoryClockRate);
-        printf("  Memory Bus Width (bits): %d\n",
-               prop.memoryBusWidth);
-        printf("  Peak Memory Bandwidth (GB/s): %f\n",
-               2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
-        int driverVersion = 0, runtimeVersion = 0;
-        cudaRuntimeGetVersion(&runtimeVersion);
-        cudaDriverGetVersion(&driverVersion); // http://rcs.bu.edu/examples/gpu/tutorials/deviceQuery/deviceQuery.cpp
-        printf("  Driver Version: %d.%d\n",driverVersion/1000, (driverVersion%100)/10);
-        printf("  Runtime Version: %d.%d\n",runtimeVersion/1000, (runtimeVersion%100)/10);
-        printf("  CUDA Version: %d.%d\n\n",  prop.major, prop.minor);
-    }
-
-
+    printDeviceDetails();
 
 	//Initialise Matrix containers
     std::cout << "Initializing.." << std::endl;
-    h_Matrix h_dx(dxElements, 8, 40, 1);
-    h_Matrix h_dy(dyElements, 8, 40, 1);
-    h_Matrix h_dz(dzElements, 3, 40, 1);
+    h_Matrix h_dx(dxElements, 8, 1, 1);
+    h_Matrix h_dy(dyElements, 8, 1, 1);
+    h_Matrix h_dz(dzElements, 3, 1, 1);
     h_Matrix h_re(reElements, 8, 8, 8);
     h_Matrix h_cc(ccElements, 1, 40, 1);
 
-    //Copy Matrix's to Device
+    cudaDeviceSetLimit(cudaLimitMallocHeapSize, 1024*1024*32);
+
+    //Copy Matrix's to    std::cout << "Device Details... \n" << std::endl;
     std::cout << "Copying to Device.." << std::endl;
     h_Matrix* d_dx = copyMatrixToDevice(&h_dx);
     h_Matrix* d_dy = copyMatrixToDevice(&h_dy);
@@ -260,52 +298,46 @@ int main() {
     h_Matrix* d_re = copyMatrixToDevice(&h_re);
     h_Matrix* d_cc = copyMatrixToDevice(&h_cc);
 
-    cublasStatus_t stat;
-    cublasHandle_t handle;
     std::cout << "Starting.." <<std::endl;
-    cublasCreate(&handle);
 
     double* cElements = new double[h_re.width]();
     h_Matrix h_c(cElements, 1, 8, 1);
-
-
     h_Matrix* d_c = copyMatrixToDevice(&h_c);
 
-    double *aMatrix;
-    cudaCheck( cudaMalloc( &aMatrix, sizeof(double) * h_re.width));
+    double *devScalar;
     double scalar = 1;
-    double* devScalar;
+
     cudaCheck( cudaMalloc(&devScalar, sizeof(double)));
     cudaCheck( cudaMemcpy(devScalar, &scalar, sizeof(double), cudaMemcpyHostToDevice));
 
 
-
-    printf("%d,%d,%d,%d,%d", h_re.width, h_dx.width,h_dx.height,h_re.height, h_dx.height);
-
     //status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 1, h_dx.height, h_re.height, &scalar, &d_dx->elements[0], h_dx.height, &d_re->elements[0], h_re.height, 0, &aMatrix[0], 1);
     //status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, h_re.height, h_re.height, 1, &scalar, &d_dx->elements[0], 1, &d_re->elements[0], 1, 0, &aMatrix[0], h_re.height);
     //MatrixMultiplyBLAS(handle, d_dx->elements, d_re->elements, aMatrix, h_dx.height, 1, h_re.width, CUBLAS_OP_T, CUBLAS_OP_N);
+    //matrixMultiplyCuda<<<1,threadsPerBlock>>>(d_dx, d_re, d_c, CUBLAS_OP_T, CUBLAS_OP_N, devScalar);
 
     dim3 threadsPerBlock(8, 8);
-    //matrixMultiplyCuda<<<1,threadsPerBlock>>>(d_dx, d_re, d_c, CUBLAS_OP_T, CUBLAS_OP_N, devScalar);
-    matrixMultiplyCuda<<<1,threadsPerBlock>>>(d_dx, d_re, d_c, 1, h_re.width, h_dx.height, CUBLAS_OP_T, CUBLAS_OP_N, devScalar);
-    double* testOutput = new double[h_dx.height * h_re.width]();
-    cudaCheck(cudaMemcpy(testOutput, h_c.devElements, sizeof(double)  * h_re.width, cudaMemcpyDeviceToHost));
-
-
-    for(int i = 0; i < h_c.height * h_c.width ; i++){
-    	std::cout << testOutput[i] << ", " << std::endl;
+    for(int i = 0; i < 1000; i++){
+    d_IPSP3d<<< 1, threadsPerBlock>>>(d_re, d_dx, d_dy, d_dz, d_cc);
     }
-    //d_IPSP3d<<< 1, 64>>>(handle, d_re, d_dx, d_dy, d_dz, d_cc);
+
+
+    //matrixMultiplyCudaKernel<<<1,threadsPerBlock>>>(d_dx, d_re, d_c, 1, h_re.width, h_dx.height, CUBLAS_OP_T, CUBLAS_OP_N, devScalar);
+   // double* testOutput = new double[h_dx.height * h_re.width]();
+
+    // Either works :)
+    //h_Matrix results = 	copyMatrixToHost(d_cc);
+    copyMatrixToHost(&h_cc, d_cc);
+
+    printf("%d,%d,%d, %f\n", h_cc.height, h_cc.width, h_cc.depth, h_cc.elements[0]);
+
+    for(int i = 0; i < h_cc.height * h_cc.width * h_cc.depth; i++){
+    	std::cout << h_cc.elements[i] << ", " << std::endl;
+    }
 
     cudaError_t err = cudaGetLastError();
     
-    printf("Error: %s\n", cudaGetErrorString(err));
-
-    //double *c = new double[1];
-    //cudaMemcpy(c, d_cc->elements, sizeof(double *), cudaMemcpyDeviceToHost);
-    //std::cout << c[0] << std::endl;
-    cublasDestroy(handle);
+    printf("\nError: %s\n", cudaGetErrorString(err));
 
     cudaFree(d_dx);
     cudaFree(d_dy);
@@ -316,3 +348,4 @@ int main() {
 }
 
 #endif
+
